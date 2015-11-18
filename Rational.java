0af9ea858d5226177261e6ca2cc65e855e2557bf
SungbinK <skim15@stuy.edu>
2015-11-18 10:09:11 -0500

    PHASE 1 COMPLETE

--- Rational.java
+++ Rational.java
@@ -3,6 +3,7 @@ public class Rational {
     private int numer;
     private int denom;
 
+    // Default Setting
     public Rational() {
 	numer = 0;
 	denom = 1;
@@ -10,6 +11,7 @@ public class Rational {
 
     public Rational(int n, int d) {
 	this();
+	// denominator cannot be zero
 	if (d == 0) {
 	    System.out.println("Invalid. Setting to 0/1");
 	}
@@ -23,6 +25,7 @@ public class Rational {
 	return numer + "/" + denom;
     }
 
+    // returns the most precise float version of the rational
     public double floatValue() {
 	return ((double)numer)/denom;
     }
